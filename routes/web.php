564545f7aa4f36fb190188be164ef128260a8b3e
Brenda Esquenazi <bresque12@gmail.com>
2026-04-29 13:22:41 -0300

    Actualizacion

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pagina-principal');
 });
 
 // Una ruta para cada página, bien clarito
